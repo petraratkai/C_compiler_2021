64cc6806e6113378b68dddc97b05e1188857b5d0
petraratkai <petraratkai18@imperial.ac.uk>
2021-03-14 12:19:04 +0000

    function constr declaration to statement

--- include/ast/ast_function.hpp
+++ include/ast/ast_function.hpp
@@ -11,11 +11,11 @@ class Function
 private:
   std::string name;
   CompoundStmt* body;
-  std::vector<Declaration*>* parameters;
+  std::vector<Statement*>* parameters;
   VarType return_type;
   std::vector<Variable_hash>* local_variables;
 public:
-  Function(const std::string& name, CompoundStmt* body, std::vector<Declaration*>* parameters, VarType return_type) :
+  Function(const std::string& name, CompoundStmt* body, std::vector<Statement*>* parameters, VarType return_type) :
       name(name), body(body), parameters(parameters), return_type(return_type) {}
   CompoundStmt* getBody() const {return body;}
 
