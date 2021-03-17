29832f5dcee99b7c0765df8d2323727a5bb5512b
Thomas van Issum <thomasvanissum@thomass-mbp.home>
2021-03-17 09:46:28 +0000

    dumb stuff

--- include/ast/Variable_hash.hpp
+++ include/ast/Variable_hash.hpp
@@ -2,8 +2,8 @@
 #define Variable_hash_hpp
 
 typedef enum VarType {IntType, VoidType, CharType, DoubleType, ShortType, LongType, FloatType, SignedType, UnsignedType, StringType} VarType;
-typedef enum VarStorage {AutoStorage, StaticStorage, ExternStorage, TypedefStorage, RegisterStorage} VarStorage;
-typedef enum VarQualifier {ConstQualifer, VolatileQualifier} VarQualifier;
+typedef enum VarStorage {AutoStorage, StaticStorage, ExternStorage, TypedefStorage, RegisterStorage, NoStorage} VarStorage;
+typedef enum VarQualifier {ConstQualifer, VolatileQualifier, NoQualifier } VarQualifier;
 class Variable_hash
 {
   std::string name;
@@ -11,13 +11,21 @@ class Variable_hash
   std::string reg; //register assigned
   int memory_address;
   bool in_memory;
+  VarStorage storage;
+  VarQualifier qualifier;
 public:
-  Variable_hash(const std::string& name, const VarType type) : name(name), type(type) {}
+  Variable_hash(const std::string& name, const VarType type) : name(name), type(type) {storage = NoStorage; qualifier = NoQualifier;}
+  Variable_hash(const std::string& name, const VarType type, const VarQualifier qualifier) : name(name), type(type), qualifier(qualifier) {storage = NoStorage;}
+  Variable_hash(const std::string& name, const VarType type, const VarStorage storage) : name(name), type(type), storage(storage) {qualifier = NoQualifier;}
+  Variable_hash(const std::string& name, const VarType type, const VarQualifier qualifier, const VarStorage storage) : name(name), type(type), qualifier(qualifier), storage(storage) {}
+
   //Variable_hash(const Variable* var, std::string reg, int memory_address, bool in_memory) : name(var->getName()), type(var->getType()), reg(reg), memory_address(memory_address), in_memory(in_memory) {}
   Variable_hash(const Variable_hash& vh)
   {
     name = vh.name;
     type = vh.type;
+    qualifier = vh.qualifier;
+    storage = vh.storage;
     reg = vh.reg;
     memory_address = vh.memory_address;
     in_memory = vh.in_memory;
@@ -26,6 +34,8 @@ public:
   std::string getReg() const {return reg;}
   std::string getName() const {return name;}
   VarType getType() const {return type;}
+  VarQualifier getQualifier() const {return qualifier;}
+  VarStorage getStorage() const {return storage;}
   bool isInMemory() const {return in_memory;}
   int getMemAddr() const {return memory_address;}
 
